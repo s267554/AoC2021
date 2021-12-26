6a4ec324dd7af400b2c13776099c28f35d237318
Filippo Fontan s267554 <s267554@studenti.polito.it>
2021-12-27 00:12:39 +0100

    maybe

--- day18/day18.py
+++ day18/day18.py
@@ -1,4 +1,5 @@
 import re
+from math import ceil, floor
 
 def sn_add(sn1, sn2):
     return f"[{sn1},{sn2}]"
@@ -8,27 +9,35 @@ def find_nearest_reg_num(sn, index_start, index_end, numToAdd, direction):
 
     skip_chars = '[],'
 
-    index_with_int = -1
+    int_start, int_end = -1, -1
     new_int = -1
+
+    num = ''
 
     if direction == 'forward':
         for i, c in enumerate(sn[index_end:]):
-            if c not in skip_chars:
-                index_with_int = index_end + i
-                new_int = int(c) + numToAdd
+            if c not in skip_chars and num == '':
+                num += c
+                int_start = index_end + i
+            elif c in skip_chars and num != '':
+                int_end = index_end + i
+                new_int = int(num) + numToAdd
                 break
     
-        return index_with_int, new_int
+        return [int_start, int_end], new_int
 
 
     if direction == 'backward':
         for i, c in enumerate(sn[:index_start][::-1]):
-            if c not in skip_chars:
-                index_with_int = len(sn[:index_start]) - i
-                new_int = int(c) + numToAdd
+            if c not in skip_chars and num == '':
+                num += c
+                int_end = len(sn[:index_start]) - i
+            elif c in skip_chars and num != '':
+                int_start = len(sn[:index_start]) - i
+                new_int = int(num[::-1]) + numToAdd
                 break
     
-        return index_with_int, new_int
+        return [int_start, int_end], new_int
 
     raise ValueError('not good')
     
@@ -40,13 +49,30 @@ def sn_explode(sn, index):
     left, right = eval(sn[index_start_reg_num : index_end_reg_num])
 
     i_new_int_l, new_int_l = find_nearest_reg_num(sn, index_start_reg_num, index_end_reg_num, numToAdd=left, direction='backward')
+    i_new_int_r, new_int_r = find_nearest_reg_num(sn, index_start_reg_num, index_end_reg_num, numToAdd=right, direction='forward')
 
-    sn_new = sn[:i_new_int_l] + str(new_int_l) + sn[i_new_int_l+1:index_start_reg_num] + '0' + sn[index_end_reg_num:i_new_int_r] + str(new_int_r) + sn[i_new_int_r+1:]
+    sn_new = '0'
 
+    if new_int_l != -1:
+        sn_new = sn[:i_new_int_l[0]] + str(new_int_l) + sn[i_new_int_l[1]:index_start_reg_num] + sn_new
+    else:
+        sn_new = sn[:index_start_reg_num] + sn_new
+    
+    if new_int_r != -1:
+        sn_new += sn[index_end_reg_num:i_new_int_r[0]] + str(new_int_r) + sn[i_new_int_r[1]:]
+    else:
+        sn_new += sn[index_end_reg_num:]
     return sn_new
 
 def sn_split(sn, index):
-    print()
+    num = int(sn[index:index+2])/2
+    left, right = floor(num), ceil(num)
+
+    sn_new = sn[:index] + f"[{left},{right}]" + sn[index+2:]
+
+    return sn_new
+
+
 
 
 def sn_reduce(sn):
@@ -54,9 +80,9 @@ def sn_reduce(sn):
         expl, spl = find4(sn), find10(sn)
         if expl == -1 and spl == -1:
             return sn
-        if spl == -1 or expl < spl:
+        if expl != -1:
             sn = sn_explode(sn, expl-1)
-        elif expl == -1 or spl < expl:
+        elif spl != -1:
             sn = sn_split(sn, spl)
         else:
             raise ValueError('WTF')
@@ -99,6 +125,7 @@ def main():
         result = sn_reduce(line)
 
     #eval string as list then calc magnitude
+    print(result)
     toCalc = eval(result)
     return magn(toCalc)
 
