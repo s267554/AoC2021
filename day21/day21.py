5a3fdae688f7e9eba19269765f34b2cbbf2be5be
Filippo Fontan s267554 <s267554@studenti.polito.it>
2021-12-26 16:44:07 +0100

    refactor

--- day21/day21.py
+++ day21/day21.py
@@ -1,39 +1,37 @@
-# no dyn prog was like 20 secs
-memo = dict()
+DICES = [1, 2, 3]
+FREQS = {i: 0 for i in range(3,10)}
+for d1 in DICES:
+    for d2 in DICES:
+        for d3 in DICES:
+            FREQS[d1+d2+d3] += 1
 
-dices = [1, 2, 3]
-freqs = dict()
-for d1 in dices:
-    for d2 in dices:
-        for d3 in dices:
-            if d1+d2+d3 not in freqs:
-                freqs[d1+d2+d3] = 1
-            else:
-                freqs[d1+d2+d3] += 1
+# best way to refactor this symmetric blobs?
+def play(pos1, pos2, score1, score2, turn):
+    if (pos1, pos2, score1, score2, turn) in memo:
+        return memo[(pos1, pos2, score1, score2, turn)]
+    univ1, univ2 = 0, 0
 
-# print(freqs)
-# only consider u1
-# got lucky
-
-
-def play(p1, p2, s1, s2, turn):
-    if (p1, p2, s1, s2, turn) in memo:
-        return memo[(p1, p2, s1, s2, turn)]
-    universes = 0
     if turn:
-        if s2 > 20:
-            return 0
-        for key, value in freqs.items():
-            tmp = (p1+key) % 10
-            universes += value * play(tmp, p2, s1+tmp+1, s2, False)
+        if score2 > 20:
+            return (0, 1)
+        for outcome, freq in FREQS.items():
+            tmp = (pos1+outcome) % 10
+            results = list(map(lambda a: a*freq, play(tmp, pos2, score1+tmp+1, score2, False)))
+            univ1 += results[0]
+            univ2 += results[1]
     else:
-        if s1 > 20:
-            return 1
-        for key, value in freqs.items():
-            tmp = (p2+key) % 10
-            universes += value * play(p1, tmp, s1, s2+tmp+1, True)
-    memo[(p1, p2, s1, s2, turn)] = universes
-    return universes
+        if score1 > 20:
+            return (1, 0)
+        for outcome, freq in FREQS.items():
+            tmp = (pos2+outcome) % 10
+            results = list(map(lambda a: a*freq, play(pos1, tmp, score1, score2+tmp+1, True)))
+            univ1 += results[0]
+            univ2 += results[1]
+
+    memo[(pos1, pos2, score1, score2, turn)] = (univ1, univ2)
+    return (univ1, univ2)
 
 # input is first two args
-print(play(3, 9, 0, 0, True))
+if __name__ == '__main__':
+    memo = dict()
+    print(max(play(3, 9, 0, 0, True)))
