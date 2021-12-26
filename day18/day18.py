c2aaad89dc0bf08c6edbf0e8b340fafd6691f366
Francesco Fontan <f.fontan@reply.it>
2021-12-26 16:45:36 +0100

    wworking on sn_explode

--- day18/day18.py
+++ day18/day18.py
@@ -3,20 +3,41 @@ import re
 def sn_add(sn1, sn2):
     return f"[{sn1},{sn2}]"
 
+
+def find_nearest_reg_num(sn, index_start, index_end, direction):
+
+    if direction == 'forward':
+        index_end_nearest = sn[index_end+2:].find(']') + index_end # +2 for ],
+        index_start_nearest = index_end + 1 # +1 for ','
+        right, left = eval(sn[index_start_nearest:index_end_nearest])
+
+    return
+
 def sn_explode(sn, index):
-    left, right = eval(sn[index : index+sn[index:].find(']')+1])
+
+    index_start_reg_num = index
+    index_end_reg_num = index+sn[index:].find(']')+1
+
+    left, right = eval(sn[index_start_reg_num : index_end_reg_num])
+
+    find_nearest_reg_num(sn, index_start_reg_num, index_end_reg_num, direction='forward')
+
+    return 
     
 
 def sn_reduce(sn):
     expl, spl = find4(sn), find10(sn)
     if expl == -1 and spl == -1:
         return sn, False
+
     if spl == -1 or expl < spl:
         sn = sn_explode(sn, expl-1)
     elif expl == -1 or spl < expl:
         sn = sn_split(sn, spl)
     else:
-        print("wtf")
+        raise ValueError('WTF')
+
+    return sn, True
 
 def find4(sn):
     par = 0
@@ -37,13 +58,29 @@ def find10(sn):
         prev = ch
     return -1    
 
-with open('test.txt') as input_file:
-    #snails = [eval(line) for line in input_file.readlines()]
-    lines = [line.strip() for line in input_file.readlines()]
 
+def main():
 
-result = lines[0]
-for line in lines[1:]:
-    line = sn_add(result, line)
-    sn_reduce(line)
-    break
+    with open('./day18/test.txt') as input_file:
+        #snails = [eval(line) for line in input_file.readlines()]
+        lines = [line.strip() for line in input_file.readlines()]
+
+    result = lines[0]
+
+    for i, line in enumerate(lines[1:]):
+        print(f'i: {i}')
+        
+        line = sn_add(result, line)
+        
+
+        while True:
+            line_reduced, is_reduced = sn_reduce(line)
+            print(f'line_reduced: {line_reduced}')
+            if is_reduced:
+                break
+
+    return
+
+if __name__ == '__main__':
+
+    main()
